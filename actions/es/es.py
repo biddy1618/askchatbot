73a668384fc25f119c84063df45384f700ce33e3
jeff.yarbro <jeff.yarbro@eduworks.com>
2022-11-22 15:09:40 -0600

    minor - bug fix

--- actions/es/es.py
+++ actions/es/es.py
@@ -253,6 +253,7 @@ def _format_result(hit: dict) -> dict:
     management      = hit.get('management'      , None  )
     question        = hit.get('question'        , None  )    
     links           = hit.get('links'           , None  )
+    label           = hit.get('label'           , None  )
     
     def _format_images(links = None):
         images = []
@@ -315,7 +316,7 @@ def _format_result(hit: dict) -> dict:
     res['body']['damage'        ] = damage
     res['body']['management'    ] = management
     res['body']['question'      ] = question
-
+    res['label'] = label
     res['images'] = _format_images(links)
     res['scores'] = _format_scores(hit  )
     
